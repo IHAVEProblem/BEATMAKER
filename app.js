37aebbbd79d5aaf4abe162b4dc532d11b9c7a370
IHAVEProblem <gataulin0202@yandex.ru>
2023-03-12 23:51:12 +0300

    moderate changes to overall look

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+// light/dark mode
+
 let lightMode = localStorage.getItem('lightMode')
 
 const lightModeToggle = document.querySelector('#light-mode-toggle')
@@ -25,3 +27,26 @@ lightModeToggle.addEventListener('click', () => {
     disableLightMode();
   }
 })
+
+
+// search
+
+const search = () => {
+  const searchBox = document.getElementById('search-item').value.toUpperCase();
+  const storeItems = document.getElementById('card-list')
+  const card = document.querySelectorAll('.card')
+  const cardName = storeItems.getElementsByTagName("p")
+
+  for (let i = 0; i < cardName.length; i++) {
+    const match = card[i].getElementsByTagName("p")[0]
+
+    if (match){
+      let textValue = match.textContent || match.innerHTML
+      if (textValue.toUpperCase().indexOf(searchBox) > -1){
+        card[i].style.display = '';
+      } else {
+        card[i].style.display = 'none';
+      }
+    }
+  }
+}
