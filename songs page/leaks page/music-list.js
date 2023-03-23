4204bd1b808b8b39842ab7049bd7e54e1296b0fd
IHAVEProblem <gataulin0202@yandex.ru>
2023-03-23 22:01:08 +0300

    folder structuring

---
+++ songs page/leaks page/music-list.js
@@ -0,0 +1,23 @@
+const leaksList = [
+  {
+    id: 1,
+    name: '20332A',
+    artist: 'HEROBRINE',
+    img: 'cover',
+    src: 'Heronwater-2033',
+  },
+  {
+    id: 2,
+    name: '20332B',
+    artist: 'HEROBRINE',
+    img: 'autumn',
+    src: 'Heronwater-2033',
+  },
+  {
+    id: 3,
+    name: '20332C',
+    artist: 'HEROBRINE',
+    img: 'xxxetanion',
+    src: 'Heronwater-2033',
+  },
+]
